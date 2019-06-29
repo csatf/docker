befb006344e98904dd5e5cd091fbc53e5adf8105
Ben Bjurstrom <ben@jelled.com>
2019-06-29 15:24:22 -0700

    Add php-ambientum and php-laravel

---
+++ php-laravel/README.md
@@ -0,0 +1,9 @@
+# benbjurstrom/php-laravel
+From ambientum/php:7.3 setup to run a laravel app in the following ways:
+- Phpfpm
+- Laravel Jobs heartbeat
+- Laravel Horizon
+- Laravel Queue Worker
+- Laravel Websockets
+
+Set $APP_MODE to one api, admin, horizon, queue, websocket, or job.
---
+++ php-laravel/init.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+##
+# exit if a command returns a non-zero exit code, print the commands and their # args as they are executed, exit if any unbound variables
+#
+set -e -x -u
+
+##
+# Run php-fpm
+#
+if [ "$APP_MODE" = "api" ]; then
+	php-fpm -F
+fi
+
+##
+# Run php-fpm
+#
+if [ "$APP_MODE" = "admin" ]; then
+	php-fpm -F
+fi
+
+##
+# Run laravel horizon
+#
+if [ "$APP_MODE" = "horizon" ]; then
+		php /var/www/artisan horizon
+fi
+
+##
+# Run a laravel queue worker
+#
+if [ "$APP_MODE" = "queue" ]; then
+		php /var/www/artisan queue:work $QUEUE_CONNECTION
+fi
+
+##
+# Run laravel websockets
+#
+if [ "$APP_MODE" = "websocket" ]; then
+	 php /var/www/artisan websockets:serve
+fi
+
+##
+# Configure a cron job to a run laravel jobs heartbeat every minute
+#
+if [ "$APP_MODE" = "job" ]; then
+	 crond
+	 echo '*  *  *  *  * php /var/www/artisan schedule:run >> /dev/null 2>&1' > /etc/crontabs/root
+	 tail -f /dev/null
+fi
+
+echo "Variable APP_MODE must be one of api, admin, horizon, queue, websocket, or job"
+exit 1
